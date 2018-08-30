436a2ec6d24c6b565e927429b176f1b9ceab148a
Antony <antoshka.lapitskiy@gmail.com>
2018-08-30 20:39:56 +0300

    adding files

---
+++ CalculateSum.php
@@ -0,0 +1,35 @@
+<?php  
+	
+	interface CalculatorInterface {
+		public function calculate();
+	}
+	
+
+	class CalculateSum implements CalculatorInterface
+	{
+		private $a;
+		private $b;
+		
+		function __construct($a, $b) {
+       		$this->a = $a;
+       		$this->b = $b;
+    	}
+
+		public function calculate() {
+			
+			$value;
+
+			if (is_string($this->a) || is_string($this->b)) {
+				
+				$value = (int)$this->a + (int)$this->b;
+
+			}else{
+
+				$value = $this->a + $this->b;
+
+			}
+			
+			return $value;
+		}
+	}
+?> 
---
+++ CalculateSumTest.php
@@ -0,0 +1,46 @@
+<?php  
+
+use CalculateSum.php;
+use PHPUnit\Framework\TestCase;
+
+class CalculateSumTest extends TestCase
+{
+	public function testCalculationOfPositiveNumbers()
+	{
+		$a = 5;
+		$b = 5;
+		$c = $a + $b;
+		$sum = new \CalculateSum($a,$b);
+		$this->assertEquals($c, $sum->calculate());
+	}
+
+	public function testCalculationOfNegativeNumbers()
+	{
+		$a = -5;
+		$b = -5;
+		$c = $a + $b;
+		$sum = new \CalculateSum($a,$b);
+		$this->assertEquals($c, $sum->calculate());
+	}
+
+	public function testCalculationOfNulls()
+	{
+		$a = null;
+		$b = null;
+		$c = $a + $b;
+		$sum = new \CalculateSum($a,$b);
+		$this->assertEquals($c, $sum->calculate());
+	}
+
+	public function testCalculationOfStrings()
+	{
+		$a = "10";
+		$b = "10";
+		$c = (int)$a + (int)$b;
+		$sum = new \CalculateSum($a,$b);
+		$this->assertEquals($c, $sum->calculate());
+	}
+}
+
+
+?>
