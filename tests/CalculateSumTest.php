1c5cf2dbcb5354226a33f6347bc49336d1e88ed6
Antony <antoshka.lapitskiy@gmail.com>
2018-08-31 17:35:31 +0300

    change

--- tests/CalculateSumTest.php
+++ tests/CalculateSumTest.php
@@ -19,10 +19,10 @@ class CalculateSumTest extends TestCase
     public function provider()
     {
         return array(
-            array(1.1, 1.1, 2.2),
+            array(1, 1, 2),
             array(-1, -1, -2),
             array(null, null, 0),
-            array('10', '10', 20)
+            array(1.1, 1.1, 2.2)
         );
     }
 }
